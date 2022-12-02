baad0c9c61fed388581aea9a403cc0a2dd38e967
Pavel Solovev <pashius2010@gmail.com>
2022-12-02 19:08:23 +0400

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
 import "./App.css";
 
 function App() {
-  return <div className="App">Pomodoro</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="settings" element={<div>Settings page</div>} />
+          <Route path="statistics" element={<div>Statistics page</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
