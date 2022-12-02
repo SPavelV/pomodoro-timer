ee69f09bf06be2e5f1247402c47a5ee6784a60e2
Pavel Solovev <pashius2010@gmail.com>
2022-12-02 16:54:49 +0400

    Home page

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Layout } from "../../components/Layout/Layout";
+import { Status } from "../../components/Status/Status";
+
+export const HomePage = () => {
+  return (
+    <Layout>
+      <div>HomePage</div>
+
+      <Status />
+    </Layout>
+  );
+};
