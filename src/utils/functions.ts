fd6367bfda378b4f97ac4c330ad8b198cbf0502c
Pavel Solovev <pashius2010@gmail.com>
2022-12-04 00:27:31 +0400

    time helper

---
+++ src/utils/functions.ts
@@ -0,0 +1,14 @@
+export const getTimeRemaining = (endtime: string) => {
+  var t = Date.parse(endtime) - Date.parse(String(new Date()));
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
+};
