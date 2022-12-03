295b31fcdb00a3a806699079c6ca6ac434960000
Pavel Solovev <pashius2010@gmail.com>
2022-12-03 20:31:37 +0400

    Status Timer

--- src/components/Time/Time.tsx
+++ src/components/Time/Time.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from "react";
 import styled from "styled-components";
+import { StatusTimer } from "../../types";
+import { getColor } from "../../utils";
 
-const Inner = styled.div`
+const Inner = styled.div<{ status: StatusTimer }>`
+  margin-bottom: 32px;
   display: flex;
   flex-flow: column wrap;
   align-items: center;
+  color: ${({ status }) => getColor(status)};
 `;
 
 const Value = styled.div`
@@ -21,11 +25,12 @@ const Value = styled.div`
 type TimeProps = {
   minutes?: number;
   seconds?: number;
+  status: StatusTimer;
 };
 
-export const Time: FC<TimeProps> = ({ minutes = 0, seconds = 0 }) => {
+export const Time: FC<TimeProps> = ({ minutes = 0, seconds = 0, status }) => {
   return (
-    <Inner>
+    <Inner status={status}>
       <Value>{minutes}</Value>
       <Value>{seconds}</Value>
     </Inner>
