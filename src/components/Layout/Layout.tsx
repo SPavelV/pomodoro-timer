4283ac23671872659f27e74f9ccf6bfb6ed44cdc
Pavel Solovev <pashius2010@gmail.com>
2022-12-03 20:27:57 +0400

    Layout bg color

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import { StatusTimer, Colors } from "../../types";
 
-const StyledLayout = styled.div`
+const StyledLayout = styled.div<{ status: StatusTimer }>`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: row wrap;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => {
+    switch (props.status) {
+      case StatusTimer.Focus:
+        return Colors.Red50;
+      case StatusTimer.LongBreak:
+        return Colors.Blue50;
+      case StatusTimer.ShortBreak:
+        return Colors.Green50;
+    }
+  }}; ;
 `;
 
 const StyledInner = styled.div`
@@ -17,11 +28,12 @@ const StyledInner = styled.div`
 
 type LayoutProps = {
   children: React.ReactNode;
+  status: StatusTimer;
 };
 
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
+export const Layout: React.FC<LayoutProps> = ({ children, status }) => {
   return (
-    <StyledLayout>
+    <StyledLayout status={status}>
       <StyledInner>{children}</StyledInner>
     </StyledLayout>
   );
