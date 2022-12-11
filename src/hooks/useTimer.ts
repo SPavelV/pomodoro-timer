efda233d32fe2b04b6c015a22ccdf7c18ba09040
Pavel Solovev <pashius2010@gmail.com>
2022-12-11 13:42:53 +0400

    Adding long break time

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,14 +1,16 @@
 import { useEffect, useState, useReducer, useRef } from "react";
 import { StatusTimer } from "../types";
 
-const FOCUS_TIME = 0.3 * 60 * 1000;
+const FOCUS_TIME = 0.2 * 60 * 1000;
 const SHORT_BREAK_TIME = 0.1 * 60 * 1000;
-const LONG_BREAK_TIME = 15 * 60 * 1000;
+const LONG_BREAK_TIME = 0.2 * 60 * 1000;
+const WORKING_PERIODS = 2;
 
 export const useTimer = () => {
   const [status, setStatus] = useState<StatusTimer>(StatusTimer.Focus);
   const [time, setTime] = useState(FOCUS_TIME);
   const [enabled, toggleEnabled] = useReducer((prev) => !prev, false);
+  const [workingPeriodCounter, setWorkingPeriodCounter] = useState(1);
   const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
   useEffect(() => {
@@ -26,18 +28,25 @@ export const useTimer = () => {
   }, [enabled]);
 
   useEffect(() => {
-    if (time <= 0 && intervalRef.current && status === StatusTimer.Focus) {
-      setTime(SHORT_BREAK_TIME);
-      setStatus(StatusTimer.ShortBreak);
+    if (time <= 0 && intervalRef.current) {
+      if (status === StatusTimer.Focus) {
+        if (workingPeriodCounter === WORKING_PERIODS) {
+          setTime(LONG_BREAK_TIME);
+          setStatus(StatusTimer.LongBreak);
+          setWorkingPeriodCounter(1);
+        } else {
+          setTime(SHORT_BREAK_TIME);
+          setStatus(StatusTimer.ShortBreak);
+          setWorkingPeriodCounter(
+            (workingPeriodCounter) => workingPeriodCounter + 1
+          );
+        }
+      } else {
+        setTime(FOCUS_TIME);
+        setStatus(StatusTimer.Focus);
+      }
     }
-  }, [time, status]);
-
-  useEffect(() => {
-    if (time <= 0 && intervalRef.current && status === StatusTimer.ShortBreak) {
-      setTime(FOCUS_TIME);
-      setStatus(StatusTimer.Focus);
-    }
-  }, [time, status]);
+  }, [time, status, workingPeriodCounter]);
 
   return { time, enabled, status, toggleEnabled } as const;
 };
