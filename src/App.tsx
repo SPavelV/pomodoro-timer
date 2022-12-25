ada58589536d25d7036bb59c5cc47ed5a8a2b9ce
Pavel Solovev <pashius2010@gmail.com>
2022-12-25 10:01:37 +0400

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,28 @@ import { HomePage } from "./pages/HomePage/HomePage";
 import { UrlRoutes } from "./types";
 import { SettingsPage } from "./pages/SettingsPage/SettingsPage";
 import { StatusProvider } from "./context/StatusContext";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider>
-        <StatusProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<HomePage />} />
-              <Route path={UrlRoutes.Settings} element={<SettingsPage />} />
-              <Route
-                path={UrlRoutes.Statistic}
-                element={<div>Statistics page</div>}
-              />
-            </Routes>
-          </BrowserRouter>
-        </StatusProvider>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider>
+          <StatusProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route index element={<HomePage />} />
+                <Route path={UrlRoutes.Settings} element={<SettingsPage />} />
+                <Route
+                  path={UrlRoutes.Statistic}
+                  element={<div>Statistics page</div>}
+                />
+              </Routes>
+            </BrowserRouter>
+          </StatusProvider>
+        </ThemeProvider>
+      </Provider>
     </div>
   );
 }
